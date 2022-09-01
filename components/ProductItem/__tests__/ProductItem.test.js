3c1aca894077f4f606d3570a396e7e89931589e7
Maxim Lebiga <lebiga.maks@gmail.com>
2022-09-01 14:13:14 +0300

    create mockdata

--- components/ProductItem/__tests__/ProductItem.test.js
+++ components/ProductItem/__tests__/ProductItem.test.js
@@ -1,35 +1,7 @@
 import ProductItem from "../ProductItem"
 import '@testing-library/jest-dom'
 import { screen, render } from '@testing-library/react'
-
-const product = {
-  node: {
-    name: 'Nikki Lime Leather Sandals',
-    node_locale: 'en',
-    thumbnailImage: {
-      file: {
-        url: '//images.ctfassets.net/bjhrajhd7nqm/5vl0dwakREcYWdzVGyhVxT/2f1bb7a0ae042011a1ffff3d8f1aff59/miista-nikki-lime-mules-6.jpg'
-      }
-    },
-    colorFamily: [
-      {
-        name: 'Green'
-      }
-    ],
-    categoryTags: ['Sandals', 'Mid-Heels', 'New Arrivals'],
-    shopifyProductEu: {
-      variants: {
-        edges: [
-          {
-            node: {
-              price: '265.00'
-            }
-          }
-        ]
-      }
-    }
-  }
-}
+import {product} from "../../../mockData"
 
 describe('product item', () => {
   it('render product item', () => {
---
+++ mockData/index.js
@@ -0,0 +1,28 @@
+export const product = {
+  node: {
+    name: 'Nikki Lime Leather Sandals',
+    node_locale: 'en',
+    thumbnailImage: {
+      file: {
+        url: '//images.ctfassets.net/bjhrajhd7nqm/5vl0dwakREcYWdzVGyhVxT/2f1bb7a0ae042011a1ffff3d8f1aff59/miista-nikki-lime-mules-6.jpg'
+      }
+    },
+    colorFamily: [
+      {
+        name: 'Green'
+      }
+    ],
+    categoryTags: ['Sandals', 'Mid-Heels', 'New Arrivals'],
+    shopifyProductEu: {
+      variants: {
+        edges: [
+          {
+            node: {
+              price: '265.00'
+            }
+          }
+        ]
+      }
+    }
+  }
+}
